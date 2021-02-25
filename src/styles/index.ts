dce8eedce44cefab1259a6418311e56f0f795ab3
Éverton Toffanetto <evertondgn@hotmail.com>
2021-02-25 05:14:18 -0300

    :lipstick: update das unidades de medida

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,7 +1,10 @@
 import styled from 'styled-components'
-import { c } from 'theme'
 
-export const ContainerMain = styled(c.Container)`
-  margin-top: 20px;
-  margin-bottom: 20px;
+export const ContainerGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(
+    auto-fit,
+    minmax(clamp(26rem, 20rem + 24vw, 24rem), 1fr)
+  );
+  grid-gap: 2rem 10rem;
 `
