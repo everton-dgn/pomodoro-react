11e7769865ca01b4b65f50476e77a4e9d0f9ae93
Éverton Toffanetto <evertondgn@hotmail.com>
2021-02-25 06:54:16 -0300

    :art: ajuste de espaçamento css

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -4,7 +4,7 @@ export const ContainerGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(
     auto-fit,
-    minmax(clamp(26rem, 20rem + 24vw, 24rem), 1fr)
+    minmax(clamp(30rem, 20rem + 27vw, 26rem), 1fr)
   );
-  grid-gap: 2rem 10rem;
+  grid-gap: 2rem calc(2rem + 2.5vw);
 `
